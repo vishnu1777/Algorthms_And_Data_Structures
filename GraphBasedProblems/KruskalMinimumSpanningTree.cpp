7c54ab10b03b1cc1e6ebc20f9750fe5c70e2debc
Vishnu <67725452+vishnu1777@users.noreply.github.com>
2023-12-28 09:53:48 +0530

    Added spanning tree and disjoint set

---
+++ GraphBasedProblems/KruskalMinimumSpanningTree.cpp
@@ -0,0 +1,79 @@
+#include<algorithm>
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+int minimumSpanningTree(vector<vector<int>>& edges, int n)
+{
+  /*
+    Don't write main().
+    Don't read input, it is passed as function argument.    
+    No need to print anything.
+    Taking input and printing output is handled automatically.
+  */
+  DisjointSet DS(n);
+  vector<pair<int,pair<int,int>>>dis;
+  for(auto it:edges){
+    int u = it[0],v = it[1],w = it[2];
+    dis.push_back({w,{u,v}});
+  }
+  sort(dis.begin(),dis.end());
+  int sum = 0;
+
+  for(auto it:dis){
+    int w = it.first;
+    int u = it.second.first;
+    int v = it.second.second;
+
+    if(DS.findUPar(u) != DS.findUPar(v)){
+      DS.unionBySize(u, v);
+      sum+=w;
+    }
+  }
+  return sum;
+}
---
+++ GraphBasedProblems/PrimsMinimumSpanningTree.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        vector<int>vis(V);
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        
+        pq.push({0,{0,-1}});
+        
+        int sum = 0;
+        
+        while(!pq.empty()){
+            auto it = pq.top(); pq.pop();
+            int wt = it.first;
+            int node = it.second.first;
+            int par = it.second.second;
+            
+            if(vis[node])continue;
+            
+            vis[node] = 1;
+            sum+=wt;
+            for(auto it:adj[node]){
+                int v = it[0], wt = it[1];
+                if(!vis[v]){
+                    pq.push({wt,{v,node}});
+                }
+            }
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        
+        Solution obj;
+    	cout << obj.spanningTree(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
