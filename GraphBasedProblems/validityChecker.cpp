fb0bfaf21130f520ca896988e9f57d673a73d064
Vishnu <67725452+vishnu1777@users.noreply.github.com>
2023-12-28 19:37:25 +0530

    Some changes in kosaraju

--- GraphBasedProblems/validityChecker.cpp
+++ GraphBasedProblems/validityChecker.cpp
@@ -2,9 +2,9 @@ bool isValid(int r,int c,int n,int m){
     return r>=0 and r<n and c>=0 and c<m;
 }
 
-// Direction vectors
-int dr[4] = {0,0,1,-1};
-int dc[4] = {1,-1,0,0};
+    // Direction vectors
+    int dr[4] = {0,0,1,-1};
+    int dc[4] = {1,-1,0,0};
 
 // 8 directions;
 int dr[8] = {0,0,1,-1,1,-1,1,-1};
