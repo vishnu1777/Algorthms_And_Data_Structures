3fdc4b005bda902daa7e61f7e57b7f4eaa096d36
Vishnu <67725452+vishnu1777@users.noreply.github.com>
2024-01-28 15:47:20 +0530

    console output cleared

--- BackTracking/MazePaths.cpp
+++ BackTracking/MazePaths.cpp
@@ -1,30 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void func(int i, int j, int n, int m, string s, unordered_set<string> &ans)
+void func(int i, int j, int n, int m, string s, vector<string> &ans,vector<vector<int>>&vis)
 {
-    if (i < 0 || j < 0 || i >= n || j >= m)
+    if ( i < 0 || j < 0 || i >= n || j >= m)
         return;
-
+    
     if (i == n - 1 and j == m - 1)
     {
-        ans.insert(s);
-        cout << s << endl;
+        ans.push_back(s);
         return;
     }
 
-    func(i + 1, j, n, m, s + "h", ans);
-
-    func(i, j + 1, n, m, s + "v", ans);
+    func(i + 1, j, n, m, s + "h", ans,vis);
+    func(i, j + 1, n, m, s + "v", ans,vis);
+    
 }
 
-void generate(int n, int m, unordered_set<string> &ans)
+void generate(int n, int m, vector<string> &ans)
 {
     string s = "";
-    func(0, 0, n, m, s, ans);
+    vector<vector<int>>vis(n,vector<int>(m));
+    func(0, 0, n, m, s, ans,vis);
 }
 
-void display(unordered_set<string> &ans)
+void display(vector<string> &ans)
 {
     for (auto s : ans)
         cout << s << " ";
@@ -35,7 +35,7 @@ int main()
 {
     int n, m;
     cin >> n >> m;
-    unordered_set<string> ans;
+    vector<string> ans;
     generate(n, m, ans);
     display(ans);
     return 0;
