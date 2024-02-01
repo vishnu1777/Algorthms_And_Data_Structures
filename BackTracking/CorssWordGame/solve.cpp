99c8770e05e3fef1db9b22bde6b7895aaee02aaa
Vishnu <67725452+vishnu1777@users.noreply.github.com>
2024-02-01 11:39:40 +0530

    feat--bugFixed in crossWord

--- BackTracking/CorssWordGame/solve.cpp
+++ BackTracking/CorssWordGame/solve.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution
 {
 public:
-    vector<vector<char>> traverseVertical(int i, int j, int nn, int m, vector<vector<char>> &grid,  string &word)
+    vector<vector<char>> traverseVertical(int i, int j, int nn, int m, vector<vector<char>> grid, string &word)
     {
         int n = word.size();
         for (int jj = 0; jj < n; jj++)
@@ -13,14 +13,15 @@ public:
             {
                 grid[i + jj][j] = word[jj];
             }
-            else{
+            else
+            {
                 grid[0][0] = '@';
                 return grid;
             }
         }
         return grid;
     }
-    vector<vector<char>>  traverseHorizontal(int i, int j, int nn, int m, vector<vector<char>> grid, string &word)
+    vector<vector<char>> traverseHorizontal(int i, int j, int nn, int m, vector<vector<char>> grid, string &word)
     {
         int n = word.size();
         for (int jj = 0; jj < n; jj++)
@@ -29,7 +30,8 @@ public:
             {
                 grid[i][j + jj] = word[jj];
             }
-            else{
+            else
+            {
                 grid[0][0] = '@';
                 return grid;
             }
@@ -39,7 +41,7 @@ public:
 
     void func(vector<string> &words, int noOfWords, int n, int m, vector<vector<char>> grid, int level)
     {
-        if(level > noOfWords)return;
+
         if (level == noOfWords)
         {
             display(grid);
@@ -47,12 +49,21 @@ public:
         }
         string word = words[level];
         int maxLen = n - word.size();
-        for (int i = 0; i < maxLen; i++)
+        // cout << "looking for word " << word << " at level:" << level << '\n';
+        for (int i = 0; i <= maxLen; i++)
         {
             for (int j = 0; j < m; j++)
             {
-                vector<vector<char>>temp = traverseVertical(i,j,n,m,grid,word);
-                if(temp[0][0] != '@')
+                if (grid[i][j] == '+')
+                    continue;
+                vector<vector<char>> temp = traverseVertical(i, j, n, m, grid, word);
+                // cout << "**Vertical Check** ";
+                // cout << "when j=" << j << endl;
+                // cout << "**Original Matrix**" << endl;
+                // display(grid);
+                // cout << "**temp**" << endl;
+                // display(temp);
+                if (temp[0][0] != '@')
                 {
                     func(words, noOfWords, n, m, temp, level + 1);
                 }
@@ -60,10 +71,20 @@ public:
         }
         for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j < maxLen; j++)
+            for (int j = 0; j <= maxLen; j++)
             {
-                vector<vector<char>>temp =  traverseHorizontal(i, j, n, m, grid, word);
-                if(temp[0][0] != '@'){
+                if (grid[i][j] == '+')
+                    continue;
+                vector<vector<char>> temp = traverseHorizontal(i, j, n, m, grid, word);
+                // cout << "**Horizontal Check**";
+                // cout << " when i=" << i << endl;
+                // cout << "**Original Matrix**" << endl;
+                // display(grid);
+                // cout << "**temp**" << endl;
+                // display(temp);
+                if (temp[0][0] != '@')
+                {
+
                     func(words, noOfWords, n, m, temp, level + 1);
                 }
             }
@@ -83,7 +104,6 @@ public:
             cout << endl;
         }
 
-        cout << endl;
     }
 };
 
